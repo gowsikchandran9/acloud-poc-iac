53eb15580b23a8fdc42cf15c8526af3d069b5f5a
Elias Abud <elias.abud@devoteam.com>
2024-02-05 11:10:47 +0100

    refactroing

--- src/python/build-lambda.sh
+++ src/python/build-lambda.sh
@@ -5,21 +5,9 @@ lambda_name=$(basename $(pwd))
 echo "It's a python lambda."
 
 mkdir function
-python3 -m venv venv
-source venv/bin/activate
-
-venv/bin/python3 -m pip install --upgrade pip
 
 cp main.py function/main.py
 cp __init__.py function/__init__.py
-
-python3 -m pip install \
-  --platform manylinux2014_x86_64 \
-  --implementation cp \
-  --python 3.12 \
-  --only-binary=:all: --upgrade \
-  --target=function \
-  -r requirements.txt
 
 cd function/
 
