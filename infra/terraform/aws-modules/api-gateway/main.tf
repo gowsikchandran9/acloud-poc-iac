ecbbb7bb9abd76152bf4787fa2ff34cd3635aead
DMAGAJI <deepthi.magaji@devoteam.com>
2024-02-06 10:54:15 +0100

    api-gateway

--- infra/terraform/aws-modules/api-gateway/main.tf
+++ infra/terraform/aws-modules/api-gateway/main.tf
@@ -5,3 +5,9 @@ resource "aws_api_gateway_rest_api" "XEntry" {
 
 
 }
+
+resource "aws_api_gateway_resource" "permission_set" {
+ rest_api_id = aws_api_gateway_rest_api.XEntry.id
+ parent_id   = aws_api_gateway_rest_api.XEntry.root_resource_id
+ path_part   = "permission-set"
+}
