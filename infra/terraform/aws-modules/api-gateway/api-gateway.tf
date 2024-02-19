2a81f575d3c55bd8c60239ee8d77012fd55217f3
Elias Abud <elias.abud@devoteam.com>
2024-02-19 12:19:05 +0100

    changed name

--- infra/terraform/aws-modules/api-gateway/api-gateway.tf
+++ infra/terraform/aws-modules/api-gateway/api-gateway.tf
@@ -1,11 +1,11 @@
-resource "aws_api_gateway_rest_api" "XEntry" {
+resource "aws_api_gateway_rest_api" "AWSEntry" {
   name        = var.api_name
   description = var.api_description
   #tags        = data.external.json_tags.result
 }
 
 resource "aws_api_gateway_resource" "permission_set" {
-  rest_api_id = aws_api_gateway_rest_api.XEntry.id
-  parent_id   = aws_api_gateway_rest_api.XEntry.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.AWSEntry.id
+  parent_id   = aws_api_gateway_rest_api.AWSEntry.root_resource_id
   path_part   = "permission-set"
 }
